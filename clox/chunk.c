a73183b5465219271b1d10509b9d20c9cc571097
Jose Barrera <j.arturo.b.g@gmail.com>
2025-07-29 20:56:29 -0600

    Add changes for stack

--- clox/chunk.c
+++ clox/chunk.c
@@ -7,28 +7,33 @@ void initChunk(Chunk* chunk){
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte){
+void writeChunk(Chunk* chunk, uint8_t byte, int line){
     // check if there is enough space to add the byte
     if (chunk->capacity < chunk->count + 1) {
         // temporarly store the current capacity
         int oldCapacity = chunk->capacity;
         // calculate the new capacity
         chunk->capacity = GROW_CAPACITY(oldCapacity);
-        // generate a new array with the calculatet capacity 
+        // generate a new array with the calculated capacity 
         // and copy the current values
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
